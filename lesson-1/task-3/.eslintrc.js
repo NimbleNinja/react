0bf6bca21b86d31aa12b97bc553a8f5dd70c1459
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-04-01 21:47:50 +0300

    lesson 1 task 3

---
+++ lesson-1/task-3/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: 'eslint-config-airbnb-base',
+  rules: {
+    'arrow-parens': 0,
+    'import/prefer-default-export': 0,
+    'comma-dangle': 0,
+  },
+};

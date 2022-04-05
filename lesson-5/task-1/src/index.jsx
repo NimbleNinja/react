3e4356533e51cbd8433b3c60c2eebf4e8c08e11e
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-04-05 16:04:16 +0300

    lesson 5 task 1

---
+++ lesson-5/task-1/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GoodButton from './GoodButton.jsx';
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<GoodButton />);

188535cc568f06a7e375aedb3e44783454cceb4e
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-04-11 19:39:45 +0300

    lesson 9 hw 2 v2

--- lesson-9/hw-2/src/Board.jsx
+++ lesson-9/hw-2/src/Board.jsx
@@ -1,70 +1,22 @@
 import React, { Component } from 'react';
 import Square from './Square';
-import HistoryItem from './HistoryItem';
-import calculateWinner from './calculateWinner.js';
 
 class Board extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-      history: [],
-    };
   }
 
-  handleClick = i => {
-    const newSquares = [...this.state.squares];
-    if (newSquares[i] || calculateWinner(newSquares)) {
-      return null;
-    }
-    newSquares[i] = this.state.xIsNext ? 'x' : 'o';
-    this.setState({
-      squares: newSquares,
-      xIsNext: !this.state.xIsNext,
-      history: [...this.state.history, newSquares],
-    });
-  };
-
-  backToStep = i => {
-    this.setState({
-      squares: this.state.history[i],
-    });
-  };
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status = winner
-      ? `Winner is: ${winner.toUpperCase()}`
-      : `Next step: ${this.state.xIsNext ? 'X' : 'O'}`;
-
     return (
-      <div className='game__body'>
-        <div className='board'>
-          {this.state.squares.map((square, index) => (
-            <Square
-              onClick={this.handleClick}
-              key={`id-${index}`}
-              id={index}
-              value={square}
-            />
-          ))}
-        </div>
-        <div className='game__conrtol-panel'>
-          <h2 className='game__player-info'>{status}</h2>
-          <ul className='game__history'>
-            {!this.state.history.length
-              ? null
-              : this.state.history.map((el, i, arr) => (
-                  <HistoryItem
-                    onClick={this.backToStep}
-                    key={`id-${i}`}
-                    step={i}
-                    countOfSteps={arr.length}
-                  />
-                ))}
-          </ul>
-        </div>
+      <div className='board'>
+        {this.props.squares.map((square, index) => (
+          <Square
+            onClick={this.props.handleClick}
+            key={`id-${index}`}
+            id={index}
+            value={square}
+          />
+        ))}
       </div>
     );
   }
--- lesson-9/hw-2/src/Game.jsx
+++ lesson-9/hw-2/src/Game.jsx
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Board from './Board';
+import HistoryItem from './HistoryItem';
+import calculateWinner from './calculateWinner.js';
 
-const Game = () => {
-  return (
-    <div className='game'>
-      <h1 className='game__title'>Tic Tac Toe</h1>
-      <Board />
-    </div>
-  );
-};
+class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      history: [],
+    };
+  }
+
+  handleClick = i => {
+    const newSquares = [...this.state.squares];
+    if (newSquares[i] || calculateWinner(newSquares)) {
+      return null;
+    }
+    newSquares[i] = this.state.xIsNext ? 'x' : 'o';
+    this.setState({
+      squares: newSquares,
+      xIsNext: !this.state.xIsNext,
+      history: [...this.state.history, newSquares],
+    });
+  };
+
+  backToStep = i => {
+    this.setState({
+      squares: this.state.history[i],
+    });
+  };
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status = winner
+      ? `Winner is: ${winner.toUpperCase()}`
+      : `Next step: ${this.state.xIsNext ? 'X' : 'O'}`;
+
+    return (
+      <div className='game'>
+        <h1 className='game__title'>Tic Tac Toe</h1>
+        <div className='game__body'>
+          <Board handleClick={this.handleClick} squares={this.state.squares} />
+          <div className='game__conrtol-panel'>
+            <h2 className='game__player-info'>{status}</h2>
+            <ul className='game__history'>
+              {!this.state.history.length
+                ? null
+                : this.state.history.map((el, i, arr) => (
+                    <HistoryItem
+                      onClick={this.backToStep}
+                      key={`id-${i}`}
+                      step={i}
+                      countOfSteps={arr.length}
+                    />
+                  ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Game;

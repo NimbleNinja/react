9613348bc2ae1f98dd9e0f7508929c7ab81b88a8
Oleg Zaporozhets <zaporozhets93@gmail.com>
2022-04-21 14:07:46 +0300

    lesson 13 task 1 fix not found

--- lesson-13/task-1/src/App.jsx
+++ lesson-13/task-1/src/App.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Products from './Products';
 import Contacts from './Contacts';
+import PageNotFound from './PageNotFound';
 
 const App = () => {
   return (
     <div className='page'>
       <BrowserRouter>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/products'>
-          <Products />
-        </Route>
-        <Route path='/contacts'>
-          <Contacts />
-        </Route>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/products'>
+            <Products />
+          </Route>
+          <Route path='/contacts'>
+            <Contacts />
+          </Route>
+          <Route path='/*'>
+            <PageNotFound />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
